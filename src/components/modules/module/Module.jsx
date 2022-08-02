3c8a4f9076870bcfcad0a0d4f2f527c2472f5c99
Ayoe <anomalous189@gmail.com>
2022-08-02 18:42:59 +0100

    final build

--- src/components/modules/module/Module.jsx
+++ src/components/modules/module/Module.jsx
@@ -9,11 +9,11 @@ const Module = ({heading, desc, price, banner}) => {
           <img src='https://static.vecteezy.com/system/resources/previews/005/608/294/original/a-happy-family-with-two-children-having-fun-while-flying-kites-on-a-family-holiday-young-family-flying-a-kite-cartoon-illustration-free-vector.jpg' alt='illustration' />
         </div>
         
-          <div className="module__text">
-            <h3>{heading}</h3>
-            <p className='module__text__desc'>{desc}</p>
-            <p className="module__text__low">For as low as</p>
-            <p className="module__text__price"><span className='price'>{price}</span> per employer</p>
+        <div className="module__text">
+          <h3>{heading}</h3>
+          <p className='module__text__desc'>{desc}</p>
+          <p className="module__text__low">For as low as</p>
+          <p className="module__text__price"><span className='price'>{price}</span> per employer</p>
         </div>
     </div>
   )
--- src/components/payment/Pay.jsx
+++ src/components/payment/Pay.jsx
@@ -5,19 +5,19 @@ const Pay = () => {
   return (
       <aside className='payment'>
         <div className='payment__top'>
-            <h3 className='payment__top__logo'>CloudletHr</h3>
-            <p className='payment__top__desc'>Industry tested no1 software solution for modern human resource providers over the world</p>
+          <h3 className='payment__top__logo'>CloudletHr</h3>
+          <p className='payment__top__desc'>Industry tested no1 software solution for modern human resource providers over the world</p>
         </div>
 
         <div className="payment__middle">
-            <h1 className="payment__middle__paying">Paying</h1> <br />
-            <span className='payment__middle__price'>N 40,000NGN</span> <br />
-            <p className='payment__middle__people'>for 300 employees</p>
+          <h1 className="payment__middle__paying">Paying</h1> <br />
+          <span className='payment__middle__price'>N 40,000NGN</span> <br />
+          <p className='payment__middle__people'>for 300 employees</p>
         </div>
 
         <div className="payment__bottom">
-            <div class="triangle-up"></div>
-            <button>Make Payment</button>
+          <div class="triangle-up"></div>
+          <button>Make Payment</button>
         </div>
 
     </aside>
